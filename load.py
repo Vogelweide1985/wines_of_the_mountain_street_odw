eed5d743f6e611a2809a92ed60720e479239a90d
Halamuda Thomas <halamuda@DOMZMG.LOCAL>
2023-03-09 10:25:21 +0100

    implementation of tokenizer and  bag of words pandas view

--- load.py
+++ load.py
@@ -7,23 +7,36 @@
 
 
 import os
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer # Bag of Words + Tokenize
+from sklearn.feature_extraction.text import TfidfTransformer # TF-IDF
 
 
 directory = "data" # directory where .txt files are stored
-text_files = [] # list to store the contents of the text files
+corpus = [] # list to store the contents of the text files
 
 # loop through files in the directory
 for filename in os.listdir(directory):
     if filename.endswith(".txt"):
         # open the file, read its contents, and append to the list
         with open(os.path.join(directory, filename), "r") as f:
-            text_files.append(f.read())
+            corpus.append(f.read())
 
 # print the contents of the list
-print(text_files)
+print(corpus)
 
 #### Tokenize ####
 
+# create a CountVectorizer object to tokenize the words
+vectorizer = CountVectorizer(analyzer  = "word")
+
+# fit the vectorizer to the text files and transform the text files into a tokenized matrix
+tokenized_matrix = vectorizer.fit_transform(corpus)
+vectorizer.get_feature_names_out()
+
+#create dataframe
+cv_dataframe=pd.DataFrame(tokenized_matrix.toarray(),columns=vectorizer.get_feature_names_out())
+print(cv_dataframe)
 
 #### TF-IDF ####
 
