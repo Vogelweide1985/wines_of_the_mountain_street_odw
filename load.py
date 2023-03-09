474b6a96bfdb187efd21c3c95c3e28fa1dad8440
Halamuda Thomas <halamuda@DOMZMG.LOCAL>
2023-03-09 08:40:00 +0100

    loaded data

--- load.py
+++ load.py
@@ -1,1 +1,31 @@
 #Init
+
+
+#Sample Data: manuell
+
+#### Load Data ####
+
+
+import os
+
+
+directory = "data" # directory where .txt files are stored
+text_files = [] # list to store the contents of the text files
+
+# loop through files in the directory
+for filename in os.listdir(directory):
+    if filename.endswith(".txt"):
+        # open the file, read its contents, and append to the list
+        with open(os.path.join(directory, filename), "r") as f:
+            text_files.append(f.read())
+
+# print the contents of the list
+print(text_files)
+
+#### Tokenize ####
+
+
+#### TF-IDF ####
+
+
+#### Cluster ####
